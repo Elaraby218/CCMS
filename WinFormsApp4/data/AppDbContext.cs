8cbf54b17a3e51b6c15c7c69ef7659994799fbbc
Elaraby218 <icpcnews5@gmail.com>
2023-12-10 01:42:43 +0200

    first commit

--- WinFormsApp4/data/AppDbContext.cs
+++ WinFormsApp4/data/AppDbContext.cs
@@ -22,8 +22,9 @@ namespace WinFormsApp4.data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-1ULGF16\\SQLEXPRESS;Initial Catalog=SSMS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-            // optionsBuilder.("Data Source=mydatabase.db");
+            // optionsBuilder.UseSqlServer("Data Source=DESKTOP-1ULGF16\\SQLEXPRESS;Initial Catalog=SSMS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            // optionsBuilder.UseSqlServer("add your connection string here");
+            optionsBuilder.UseSqlite("Data Source=mydatabase.db");
         }
     }
 }
