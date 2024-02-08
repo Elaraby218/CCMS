118828415b94303d1a2d90a416941d2193ca0c4c
Omar7001-B <Omar.20375710@compit.aun.edu.eg>
2024-02-08 20:22:53 +0200

    Changed Folder Name to CCMS

--- WinFormsApp4/Properties/Resources.Designer.cs
+++ WinFormsApp4/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsApp4.Properties {
+namespace CCMS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormsApp4.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsApp4.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CCMS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
