82076129ac094c508e345ac12fb23de8c8c1343a
Omar7001-B <Omar.20375710@compit.aun.edu.eg>
2024-02-08 20:41:54 +0200

    Revert "Changed Folder Name to CCMS"
    This reverts commit 118828415b94303d1a2d90a416941d2193ca0c4c.

--- WinFormsApp4/Properties/Resources.Designer.cs
+++ WinFormsApp4/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CCMS.Properties {
+namespace WinFormsApp4.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CCMS.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CCMS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsApp4.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
